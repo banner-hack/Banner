a400ada40c91185c8b1d607ec0b9b4c2b18332f0
KeitaroKawahara <keitaro.inc@gmail.com>
2023-05-27 01:27:52 +0900

    Initial Commit

---
+++ Banner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banner
+//
+//  Created by Keitaro Kawahara on 2023/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
