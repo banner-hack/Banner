5850c4dc67bf2de2b769db0d321c97ecc457ec0d
KeitaroKawahara <keitaro.inc@gmail.com>
2023-05-27 02:27:15 +0900

    Storyboardを削除しXibファイルが起動するように変更

--- Banner/HomeViewController.swift
+++ Banner/HomeViewController.swift
@@ -8,13 +8,19 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    init() {
+        super.init(nibName: R.nib.homeViewController.name, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
 
     /*
     // MARK: - Navigation
@@ -25,5 +31,4 @@ class HomeViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
