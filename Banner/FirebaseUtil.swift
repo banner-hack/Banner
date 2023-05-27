da9a6b4b7344ac3fec99977e34e8bde2c5569e65
KeitaroKawahara <keitaro.inc@gmail.com>
2023-05-27 14:53:21 +0900

    restaurantsModelファイルを作成

--- Banner/FirebaseUtil.swift
+++ Banner/FirebaseUtil.swift
@@ -5,4 +5,17 @@
 //  Created by Keitaro Kawahara on 2023/05/27.
 //
 
+import FirebaseCore
+import FirebaseFirestore
 import Foundation
+
+struct FirebaseUtil {
+    var db = Firestore.firestore()
+
+    var docRef = db.collection("Restaurants")
+
+    func addDocument() {
+//        try await db.collection("Users").addDocument(from: )
+
+    }
+}
---
+++ Banner/RestaurantsModel.swift
@@ -0,0 +1,8 @@
+//
+//  RestaurantsModel.swift
+//  Banner
+//
+//  Created by Keitaro Kawahara on 2023/05/27.
+//
+
+import Foundation
