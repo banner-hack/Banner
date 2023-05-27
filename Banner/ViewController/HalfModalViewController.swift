f7a33677695661ca34a52889bd1a08cce4ba1e79
KeitaroKawahara <keitaro.inc@gmail.com>
2023-05-28 02:49:54 +0900

    firebase上のデータをhalfModalViewに表示

--- Banner/ViewController/HalfModalViewController.swift
+++ Banner/ViewController/HalfModalViewController.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-class HalfModalViewController: UIViewController, UIAdaptivePresentationControllerDelegate {
+final class HalfModalViewController: UIViewController, UIAdaptivePresentationControllerDelegate {
+    @IBOutlet var leftLabel: UILabel!
+    @IBOutlet var rightLable: UILabel!
+    private var firebaseUtil = FirebaseUtil()
+    private var restaurantsData: [Restaurants] = []
+
     init() {
         super.init(nibName: R.nib.halfModalViewController.name, bundle: nil)
     }
@@ -18,9 +23,31 @@ class HalfModalViewController: UIViewController, UIAdaptivePresentationControlle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setUpView()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+    }
+}
+
+private extension HalfModalViewController {
+    func setUpView() {
+        Task {
+            do {
+                 restaurantsData = try await firebaseUtil.getDocuments()
+                print(restaurantsData)
+                guard
+                    let leftData = restaurantsData.first?.name,
+                    let rightData = restaurantsData.last?.name
+                else {
+                    return
+                }
+                self.leftLabel.text = restaurantsData.first?.name
+                self.rightLable.text = restaurantsData.last?.name
+            } catch {
+                print("Failed to retrieve document data: \(error.localizedDescription)")
+            }
+        }
     }
 }
