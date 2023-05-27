7f847820457cfa9d5f270dfb1a8d1daa0c980dc8
KeitaroKawahara <keitaro.inc@gmail.com>
2023-05-27 12:26:51 +0900

    コンフリクトを解消

--- Banner/HomeViewController.swift
+++ Banner/HomeViewController.swift
@@ -12,6 +12,7 @@ import CoreLocation
 
 <<<<<<< HEAD
 final class HomeViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var mapView: MKMapView!
     @IBOutlet var getLocationButton: UIButton!
     private var locationManager = CLLocationManager()
 
@@ -27,7 +28,7 @@ final class HomeViewController: UIViewController, CLLocationManagerDelegate {
         super.viewDidLoad()
 
         locationManager.delegate = self
-
+        setupMapView()
         print("CLLocationManager.authorizationStatus=\(CLLocationManager.authorizationStatus().description)")
     }
 
@@ -72,28 +73,14 @@ extension HomeViewController {
             return
         }
         var locationData = Location(latitude: latitude, longitude: longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        mapView.showsUserLocation = true
+        mapView.setRegion(region, animated: true)
         print(location)
     }
 
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("didFailWithError error=\(error.localizedDescription)")
-    }
-}
-
-private extension HomeViewController {
-    @IBAction func getLocation() {
-        let locationModel = LocationModel()
-        locationModel.setUpLocation()
-        //        locationModel.locationManagerDidChangeAuthorization(locationManage)
-=======
-class HomeViewController: UIViewController {
-    @IBOutlet weak var mapView: MKMapView!
-    private let locationManager = CLLocationManager()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupMapView()
-        checkLocationAuthorization()
     }
     
     private func setupMapView() {
@@ -116,38 +103,12 @@ class HomeViewController: UIViewController {
         mapView.addAnnotation(annotation)
     }
     
-    private func checkLocationAuthorization() {
-        let authorizationStatus = CLLocationManager.authorizationStatus()
-        
-        if authorizationStatus == .authorizedWhenInUse || authorizationStatus == .authorizedAlways {
-            mapView.showsUserLocation = true
-        } else if authorizationStatus == .denied || authorizationStatus == .restricted {
-            // 位置情報の使用が許可されていない場合の処理
-            // 必要なエラーハンドリングやユーザーへの案内を追加してください
-        } else if authorizationStatus == .notDetermined {
-            locationManager.requestWhenInUseAuthorization()
-        }
-    }
 }
 
-extension HomeViewController: CLLocationManagerDelegate {
-    // 位置情報の更新があった場合に呼ばれるメソッド
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
-                                            longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        mapView.setRegion(region, animated: true)
-        locationManager.stopUpdatingLocation() // 一度現在地が表示されたら更新を停止する
-    }
-    
-    // 位置情報の使用許可状態が変更された場合に呼ばれるメソッド
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if status == .authorizedWhenInUse {
-            mapView.showsUserLocation = true
-            locationManager.delegate = self
-            locationManager.startUpdatingLocation()
-        }
->>>>>>> 9707c651527454a61e84cc4c61473731960e78ea
+private extension HomeViewController {
+    @IBAction func getLocation() {
+        let locationModel = LocationModel()
+        locationModel.setUpLocation()
+        //        locationModel.locationManagerDidChangeAuthorization(locationManage)
     }
 }
